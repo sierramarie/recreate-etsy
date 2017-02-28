fcfaea2f6232ac3b03972e4a3960f1a7911c472a
Sierra <sierramroberts.96@gmail.com>
2017-02-28 16:29:43 -0500

    start

---
+++ js/global.js
@@ -0,0 +1,19 @@
+// Rebuild an Etsy search results page using HTML, CSS, & a JavaScript function that creates and displays each result using an object. This assignment requires that you know how to work with the DOM.
+
+
+// cards 
+
+// createCard({
+//     src: 'http://unsplash.it/400/300?image=500',
+//     caption: 'Unknown Image'
+// });
+
+// function createCard(image) { 
+//    var card =`<div class="card">
+//         <img src="${image.src}" alt="Image"/>
+//         <h5 class="card-caption">${image.caption}</h5>
+//     </div>`;
+
+//     document.querySelector('#app').innerHTML += card;
+//     // calls the app and puts the HTML data in it
+// };
