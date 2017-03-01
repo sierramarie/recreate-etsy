6a7891bc2f2dc79fc253745b9c95da76877b4725
Sierra <sierramroberts.96@gmail.com>
2017-03-01 16:51:00 -0500

    Data

--- js/global.js
+++ js/global.js
@@ -1,19 +1,48 @@
 // Rebuild an Etsy search results page using HTML, CSS, & a JavaScript function that creates and displays each result using an object. This assignment requires that you know how to work with the DOM.
+var etsy = {};
+
+getData();
+
+function getData() {
+    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('board games') + '&includes=Images,Shop')
+        .then(response => response.json())
+        .then(data => {
+            etsy = data;
+
+            console.log(etsy);
+
+            renderItemCard();
+        });
+}
+
+function renderItemCard() {
+    etsy.results.forEach(createSearchResultCard)
+}
 
 
-// cards 
+// for (var i = 0; i < 12; i++) {
+//     createSearchResultCard({
+//         src: 'http://unsplash.it/600?random',
+//         description: 'Some product description...',
+//         seller: 'HBakerStudio',
+//         price: '$18.00'
+//     });
+// }
 
-// createCard({
-//     src: 'http://unsplash.it/400/300?image=500',
-//     caption: 'Unknown Image'
-// });
 
-// function createCard(image) { 
-//    var card =`<div class="card">
-//         <img src="${image.src}" alt="Image"/>
-//         <h5 class="card-caption">${image.caption}</h5>
-//     </div>`;
+function createSearchResultCard(data) {
+    var card = `<div class="col-sm-4 col-md-3">
+        <div class="card">
+            <img src="${data.Images[0].url_170x135}"/>
+            <div class="card-caption">
+                <div>${data.title.slice(0, 60)}...</div>
+                <div>
+                    <span class="text-muted">${data.Shop.shop_name}</span>
+                    <span class="text-success pull-right">${data.price}</span>
+                </div>
+            </div>
+        </div>
+    </div>`;
 
-//     document.querySelector('#app').innerHTML += card;
-//     // calls the app and puts the HTML data in it
-// };
+    document.querySelector('#cards').innerHTML += card;
+}
